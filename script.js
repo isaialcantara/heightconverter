a4ba9bb5f0c9509ca04cd888efc777c9621442c7
Isai Alcântara <11619375+isfalcantara@users.noreply.github.com>
2020-04-07 21:47:44 -0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,37 @@
+function convert(event) {
+  var feet = document.getElementById("feet");
+  var inches = document.getElementById("inches");
+  var centimeters = document.getElementById("centimeters");
+  var decimals = document.getElementById("decimals");
+
+  feet.value = Math.max(0, feet.value);
+  inches.value = Math.max(0, inches.value);
+  inches.value = Math.min(11, inches.value);
+  centimeters.value = Math.max(0, centimeters.value);
+  decimals.value = Math.max(0, decimals.value);
+
+  switch (event.target.id) {
+    case "feet":
+    case "inches":
+      centimeters.value = +(feet.value * 30.48 + inches.value * 2.54).toFixed(
+        decimals.value
+      );
+      break;
+    case "centimeters":
+      feet.value = +(~~(centimeters.value / 30.48)).toFixed(decimals.value);
+      inches.value = +((centimeters.value % 30.48) / 2.54).toFixed(
+        decimals.value
+      );
+      break;
+
+    case "decimals":
+      centimeters.value = +(+centimeters.value).toFixed(decimals.value);
+      feet.value = +(~~(centimeters.value / 30.48)).toFixed(decimals.value);
+      inches.value = +((centimeters.value % 30.48) / 2.54).toFixed(
+        decimals.value
+      );
+      break;
+    default:
+      break;
+  }
+}
